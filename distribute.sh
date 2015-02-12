0fd9b3ba25bc059ac1294e7689bdb63f7cae463a
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2015-02-12 21:23:32 +0900

    Add distribute script

---
+++ distribute.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -s  ${PWD}/.vimrc ~/.vim 
+ln -s  ${PWD}/.vimrc ~/.vimrc 
+ln -s  ${PWD}/.vimrc ~/.bash_profile 
+ln -s  ${PWD}/.vimrc ~/.bash_logout 
+ln -s  ${PWD}/.vimrc ~/.gitconfig 
