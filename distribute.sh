f09cf90c7df48e652639590a7126561f79578071
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2015-07-05 03:42:27 +0900

    Add .gitignore symlink

--- distribute.sh
+++ distribute.sh
@@ -4,4 +4,5 @@ ln -s  ${PWD}/.vim ~/.vim
 ln -s  ${PWD}/.vimrc ~/.vimrc 
 ln -s  ${PWD}/.bash_profile ~/.bash_profile 
 ln -s  ${PWD}/.bash_logout ~/.bash_logout 
+ln -s  ${PWD}/.gitignore ~/.gitignore 
 ln -s  ${PWD}/.gitconfig ~/.gitconfig 
