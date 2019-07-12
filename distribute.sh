a91060ec663c87556fe81d743c5b4c9d9f07e78a
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2019-07-13 00:09:08 +0900

    Completed to convert by mackup

--- distribute.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -s  ${PWD}/.vimrc ~/.vimrc 
-ln -s  ${PWD}/.bash_profile ~/.bash_profile 
-ln -s  ${PWD}/.bash_logout ~/.bash_logout 
-ln -s  ${PWD}/.gitignore ~/.gitignore 
-ln -s  ${PWD}/.gitconfig ~/.gitconfig 
-ln -s  ${PWD}/.tmux.conf ~/.tmux.conf
--- removeLinks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rm ~/.vimrc ~/.bash_profile ~/.bash_logout ~/.gitconfig ~/.tmux.conf
