2498d2941bb0ccfbfece3379b5c1174f0e7bd2bc
jupemara <jumpei.arashi@arashike.com>
2025-04-15 16:57:24 +0900

    color_schema
    https://wezterm.org/colorschemes/s/index.html#solarized-dark-higher-contrast-gogh

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,13 @@
-local wezterm = requiire 'wezterm'
+local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_schem = 'AdventureTime'
+config.automatically_reload_config = true
+config.font_size = 15
+config.color_scheme = 'Solarized Dark Higher Contrast (Gogh)'
+config.use_ime = true
+config.font_dirs = { 'fonts' }
+config.font = wezterm.font(
+  "HackGen Console NF"
+)
 
 return config
