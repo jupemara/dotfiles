9e1a60ea2f514558032b49606e706c6076305e22
jupemara <jumpei.arashi@arashike.com>
2025-04-15 18:32:22 +0900

    vim cursor line visibility++

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,11 +3,20 @@ local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
 config.font_size = 15
-config.color_scheme = 'Solarized Dark Higher Contrast (Gogh)'
+config.color_scheme = 'Solarized Dark (Gogh)'
 config.use_ime = true
-config.font_dirs = { 'fonts' }
-config.font = wezterm.font(
-  "HackGen Console NF"
-)
+
+-- fonts
+
+config.font = wezterm.font_with_fallback{
+  "HackGen Console NF",
+  "JetBrains Mono",
+}
+
+-- blink
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+config.cursor_blink_rate = 512
+config.default_cursor_style = "BlinkingBlock"
 
 return config
